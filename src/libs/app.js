2b2f7dd97489d20e6c1ab0c06226a084f3ebb338
Alexandre Masy <hello@alexandremasy.com>
2017-05-25 11:36:55 +0200

    Implement fancybox

--- src/libs/app.js
+++ src/libs/app.js
@@ -3,6 +3,25 @@
 
   jQuery(window).on('load', function()
   {
+    $('a[data-fancybox="cl-group"]').fancybox({
+
+		    baseClass : 'fancybox-custom-layout',
+        margin    : 0,
+		    infobar   : false,
+        thumbs    : {
+          hideOnClosing : false
+        },
+        touch : {
+          vertical : 'auto'
+        },
+        closeClickOutside : false,
+
+        // Customize caption area - append an ad to the bottom
+        caption : function( instance ) {
+          var ad = '<div class="ad"><p><a href="//fancyapps.com/fancybox/">fancyBox3</a> - touch enabled, responsive and fully customizable lightbox script</p></div>';
+          return ad + ( $(this).data('caption') || '' );
+        }
+	});
 
   });
 
